05a6ba6807cab4c6d01e0dd176dd24e13d9aa839
Dave M <dave.nerd@gmail.com>
2016-10-10 07:35:34 -0500

    begin initial Prompt.pm

--- Shredder/Prompt.pm
+++ Shredder/Prompt.pm
@@ -15,47 +15,35 @@ package Shredder::Prompt;
 
 use strict;
 use warnings;
+use Gtk3 '-init';
 use Glib 'TRUE', 'FALSE';
 $| = 1;
 
+show_window();
+
 sub show_window {
-    my $window = Gtk3::Window->new( 'toplevel' );
-    $window->signal_connect( destroy => sub { Gtk3->main_quit } );
-    $window->set_default_size( 300, 300 );
-    $window->set_border_width( 10 );
+    my $dialog = Gtk3::Dialog->new_with_buttons( 'title', undef,
+        'destroy-with-parent', );
+    $dialog->add_buttons( 'gtk-cancel', 'cancel', 'gtk-ok', 'ok' );
+    $dialog->set_title( 'title goes here' );
+    $dialog->signal_connect( destroy => sub { Gtk3->main_quit } );
+    $dialog->set_default_size( 300, 300 );
+    #$dialog->set_border_width( 10 );
 
     my $box = Gtk3::Box->new( 'vertical', 5 );
-    $window->add( $box );
+    $dialog->get_content_area->add( $box );
 
     my $header = Gtk3::HeaderBar->new;
-    $window->set_titlebar( $header );
+    $dialog->set_titlebar( $header );
     $header->set_title( 'File shredder' );
     $header->set_subtitle( 'Settings' );
     $header->set_show_close_button( TRUE );
     $header->set_decoration_layout( 'menu:minimize,close' );
 
-    my $btn = Gtk3::Button->new_from_icon_name( 'gtk-dialog-question', 3 );
-    $btn->signal_connect( clicked => \&info );
-    $header->add( $btn );
-    $btn = Gtk3::Button->new_from_icon_name( 'gtk-about', 3 );
-    $btn->signal_connect( clicked => \&about );
-    $header->add( $btn );
-    $btn = Gtk3::Button->new_from_icon_name( 'gtk-quit', 3 );
-    $btn->signal_connect( clicked => sub { Gtk3->main_quit } );
-    $header->add( $btn );
-
-    my $grid = Gtk3::Grid->new;
-    $grid->set_row_spacing( 5 );
-    $grid->set_column_spacing( 10 );
-    $grid->set_column_homogeneous( TRUE );
-    $box->pack_start( $grid, FALSE, FALSE, 10 );
-
-    # For popover
-    my $label = Gtk3::Label->new( 'Saved' );
-    $label->show;
-
-    $window->show_all;
-    Gtk3->main;
+    $dialog->show_all;
+    $dialog->run;
+    $dialog->destroy;
+    Gtk3->main_quit;
 }
 
 1;
