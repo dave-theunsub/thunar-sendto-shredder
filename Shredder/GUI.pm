41be34185870b115afb9da8c8bde9cafd858cca0
Dave M <dave.nerd@gmail.com>
2016-10-10 09:07:20 -0500

    moved Prompt.pm stuff into GUI.pm

--- Shredder/GUI.pm
+++ Shredder/GUI.pm
@@ -25,17 +25,15 @@ use File::Find;
 my @objects = ();
 
 # Our Gtk3::ProgressBar; global for now
-my $pb = '';
-
-# Don't need this
-my $pb_step = '';
-
-# Either for figuring out the step or couting how
-# many were shredded overall
+my $pb            = '';
+my $pb_step       = 0;
 my $pb_file_count = 0;
 
 # Status label; shows user what is happening
 my $label = '';
+
+my $files_seen    = 0;
+my $files_deleted = 0;
 
 sub show_window {
     @objects = @_;
@@ -54,12 +52,7 @@ sub show_window {
     $header->set_show_close_button( TRUE );
     $header->set_decoration_layout( 'menu:minimize,close' );
 
-    my $btn = Gtk3::Button->new_from_icon_name( 'gtk-about', 3 );
-    $btn->set_tooltip_text( 'About this program' );
-    $btn->signal_connect( clicked => \&about );
-    $header->pack_start( $btn );
-
-    $btn = Gtk3::Button->new_from_icon_name( 'gtk-quit', 3 );
+    my $btn = Gtk3::Button->new_from_icon_name( 'gtk-quit', 3 );
     $btn->set_tooltip_text( 'Quit this program' );
     $btn->signal_connect( clicked => sub { Gtk3->main_quit } );
     $header->pack_start( $btn );
@@ -67,13 +60,15 @@ sub show_window {
     $label = Gtk3::Label->new( '' );
     $box->pack_start( $label, FALSE, FALSE, 0 );
 
-    $pb            = Gtk3::ProgressBar->new;
-    $pb_step       = 0;
-    $pb_file_count = 0;
+    $pb = Gtk3::ProgressBar->new;
     $box->pack_start( $pb, FALSE, FALSE, 0 );
     $window->{ pb } = $pb;
 
-    shred();
+    if ( Shredder::Config::get_conf_value ) {
+        prompt();
+    } else {
+        shred();
+    }
 
     $window->show_all;
     Gtk3->main;
@@ -81,12 +76,14 @@ sub show_window {
 
 sub shred {
     my $options = ' -D';
-    my $count   = 0;
+
+    my $files_seen    = 0;
+    my $files_deleted = 0;
 
     for my $o ( @objects ) {
         warn "in for loop: o = >$o<\n";
+        $files_seen += 1;
         if ( -f $o ) {
-            $pb_file_count += 1;
         } elsif ( -d $o ) {
             $options .= ' -R';
             find( { wanted => \&wanted, preprocess => \&nodirs }, $o );
@@ -171,7 +168,42 @@ sub get_shred_path {
 }
 
 sub prompt {
+    my $num_of_files = shift;
+    warn "no files?\n" and return unless ( $num_of_files );
 
+    my $dialog = Gtk3::Dialog->new_with_buttons( 'title', undef,
+        'destroy-with-parent', );
+    $dialog->add_buttons( 'gtk-cancel', 'cancel', 'gtk-ok', 'ok' );
+    $dialog->set_title( 'title goes here' );
+    $dialog->signal_connect( destroy => sub { Gtk3->main_quit } );
+    $dialog->set_default_size( 200, 200 );
+    $dialog->set_border_width( 10 );
+
+    my $box = Gtk3::Box->new( 'vertical', 5 );
+    $dialog->get_content_area->add( $box );
+
+    my $header = Gtk3::HeaderBar->new;
+    $dialog->set_titlebar( $header );
+    $header->set_title( 'File shredder' );
+    $header->set_subtitle( 'Settings' );
+    $header->set_show_close_button( TRUE );
+    $header->set_decoration_layout( 'menu:minimize,close' );
+
+    my $phrase = sprintf(
+        "You are about to permanently erase %d files. " . "\n\n"
+            . "Press OK to continue or cancel to stop this operation.",
+        $num_of_files
+    );
+    my $label = Gtk3::Label->new( $phrase );
+    $box->add( $label );
+
+    $dialog->show_all;
+    if ( $dialog->run eq 'cancel' ) {
+        warn "pressed cancel!\n";
+        $dialog->destroy;
+        return;
+    }
+    $dialog->destroy;
 }
 
 sub warning {
